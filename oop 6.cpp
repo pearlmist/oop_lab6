6e3b1fe1a833fbdb6168b69cc1e8c164cc70c85c
pearlmist <90666519+pearlmist@users.noreply.github.com>
2021-12-14 15:01:41 +1000

    Add files via upload

--- oop 6.cpp
+++ oop 6.cpp
@@ -7,7 +7,7 @@ struct product {
 	int price;
 };
 void print_product(product print_product) {
-	cout << "Название блюда: " << print_product.name << " Цена: " << print_product.price;
+	cout << "Название блюда: " << print_product.name << "\n" << "Цена: " << print_product.price;
 }
 
 class catering
@@ -15,17 +15,44 @@ class catering
 public:
 	catering();
 	~catering();
-	void set_location(double x, double y) {
+	void set_location() {
+		double x, y;
+		cout << "Местонахождение точки по оси x: ";
+		cin >> x;
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(32767, '\n');
+			cout << "Ошибка при заполнении местонахождения\n";
+			this->location_x = 0;
+			this->location_y = 0;
+			return;
+		}
+		cout << "Местонахождение точки по оси y: ";
+		cin >> y;
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(32767, '\n');
+			cout << "Ошибка при заполнении местонахождения\n";
+			this->location_x = 0;
+			this->location_y = 0;
+			return;
+		}
 		this->location_x = x;
 		this->location_y = y;
 	};
 
-	void set_rating(int rate) {
-		if ((rate >= 0) and (rate <= 5))
-			this->rating = rate;
-		else {
-			cout << "Неверный рейтинг";
-		};
+	void set_rating() {
+		cout << "Рейтинг точки от 0 до 5: ";
+		int rate;
+		cin >> rate;
+		if ((cin.fail()) and (rate <= 5) and (rate >= 0)) {
+			cin.clear();
+			cin.ignore(32767, '\n');
+			cout << "Ошибка при заполнении рейтинга\n";
+			this->rating = 0;
+			return;
+		}
+		this->rating = rate;
 		return;
 	};
 
@@ -37,7 +64,17 @@ public:
 		return this->rating;
 	};
 
-	void set_averprice(int price) {
+	void set_averprice() {
+		int price;
+		cout << "Средняя цена заведения: ";
+		cin >> price;
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(32767, '\n');
+			cout << "Ошибка при заполнении среднего чека\n";
+			this->aver_price = 0;
+			return;
+		}
 		this->aver_price = price;
 	};
 
@@ -51,15 +88,18 @@ public:
 			cout << "Название блюда: ";
 			cin >> new_product.name;
 			if (cin.fail()) {
-				cin.clear();
+				//cin.clear();
+				cin.ignore(32767, '\n');
 				cout << "Ошибка при заполнении\n";
 				return;
 			}
+			cin.ignore(32767, '\n');
 			cout << "Цена: ";
 			cin >> new_product.price;
 			if (cin.fail()) {
 				cin.clear();
-				cout << "Ошибка при заполнении местонахождения\n";
+				cin.ignore(32767, '\n');
+				cout << "Ошибка при заполнении\n";
 				return;
 			}
 			this->menu[pos_in_menu] = new_product;
@@ -73,10 +113,31 @@ public:
 	};
 
 	void print_menu() {
-		for (int i = 1; i < pos_in_menu; i++) {
+		for (int i = 0; i < pos_in_menu; i++) {
+			cout << "Блюдо " << i + 1 << ":" << "\n";
 			print_product(menu[i]);
+			cout << "\n";
 		}
 	};
+	void create_name() {
+		string name;
+		cout << "Название точки питания: ";
+		cin >> name;
+		cin.ignore(32767, '\n');
+		this->name_catering = name;
+	};
+	void change_type(int type) {
+		this->type_class = type;
+	}
+
+	unsigned char get_type() {
+		return this->type_class;
+	}
+
+	friend void print_class(const catering& catering);
+	//friend class cafe;
+	//friend class dining_room;
+	virtual const char* getName() { return "catering"; }
 
 private:
 	string name_catering;
@@ -86,51 +147,24 @@ private:
 	int aver_price;
 	int pos_in_menu;
 	product menu[25];
+	unsigned char type_class;
 };
 
 catering::catering()
 {
-	string name;
-	cout << "Название точки питания: ";
-	cin >> name;
-	double x, y;
-	cout << "Местонахождение точки по оси x: ";
-	cin >> x;
-	if (cin.fail()) {
-		cin.clear();
-		cout << "Ошибка при заполнении местонахождения\n";
-		return;
-	}
-	cout << "Местонахождение точки по оси y: ";
-	cin >> y;
-	if (cin.fail()) {
-		cin.clear();
-		cout << "Ошибка при заполнении местонахождения\n";
-		return;
-	}
-	int price;
-	cout << "Средняя цена заведения: ";
-	cin >> price;
-	if (cin.fail()) {
-		cin.clear();
-		cout << "Ошибка при заполнении среднего чека\n";
-		return;
-	}
-	cout << "Рейтинг точки от 1 до 5: ";
-	int rate;
-	cin >> rate;
-	set_location(x,y);
-	set_rating(rate);
-	set_averprice(price);
-	this->name_catering = name;
+	create_name();
+	set_location();
+	set_averprice();
+	set_rating();
 	this->pos_in_menu = 0;
 }
 
 catering::~catering()
 {
-	set_location(0, 0);
-	set_rating(0);
-	set_averprice(0);
+	this->location_x = 0;
+	this->location_y = 0;
+	this->rating = 0;
+	this->aver_price = 0;
 	this->pos_in_menu = 0;
 	for (int i = 0; i < 25; i++) {
 		menu[i].name = ' ';
@@ -139,14 +173,48 @@ catering::~catering()
 	this->name_catering = ' ';
 }
 
+class cafe : public catering
+{
+public: //1 - кафе
+	cafe() {
+		change_type(1);
+	}
+	virtual const char* getName() { return "cafe"; }
+	
+};
+class dining_room : public catering
+{
+public: //2 - столовая
+	dining_room() {
+		change_type(2);
+	}
+	virtual const char* getName() { return "dining_room"; }
+};
 
+void print_class(const catering& catering) {
+	cout << "Тип заведения: ";
+	if (catering.type_class == 1) cout << "кафе";
+	if (catering.type_class == 2) cout << "столовая";
+	cout << "\n";
+}
 
 int main()
 {
 	setlocale(LC_ALL, "ru_RU");
-	catering a;
+	cafe a;
+	print_class(a);
+	dining_room b;
+	print_class(b);
+	catering& example = a;
+	cout << "\n--- " << example.getName() << " ---\n";
 	a.get_location();
 	cout << "Рейтинг: " << a.get_rating() << "\n";
 	cout << "Средний чек: " << a.get_price() << "\n";
+	a.add_product();
+	a.add_product();
+	a.print_menu();
+	if (a.get_type() == 1) cout << "Тип: кафе";
+	else if (a.get_type() == 2) cout << "Тип: столовая";
+
 
 }
